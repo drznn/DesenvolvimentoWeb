0fb1c34cd94f8a6681c5ffa440f24b28863dda01
André Silva Cavalcanti de Albuquerque <103077110+drznn@users.noreply.github.com>
2022-06-08 19:37:51 -0300

    Update tzeet.js

--- Lista 4/tzeet.js
+++ Lista 4/tzeet.js
@@ -1,20 +1,32 @@
-function caracteres(e) {
-    let count = document.getElementById('contChar')
+function caracteres(e, id) {
+    var context, button = '';
+
+    if(id === 'storyHome') {
+        context = 'contChar';
+        button = 'botaoEnviarHome';
+    } else if (id === 'storyModal') {
+        context = 'contCharModal';
+        button = 'botaoEnviarModal';
+    }
+
+    console.log(button)
+    let count = document.getElementById(context);
+    let buttonElement = document.getElementById(button);
     if(e.target.value.length === 0){
         count.style.opacity = 0;
-        document.getElementById("botaoEnviar").disabled = true;
+        buttonElement.disabled = true;
     }else{
-        document.getElementById("botaoEnviar").disabled = false;
+        buttonElement.disabled = false;
         count.style.opacity = 1;
         if(e.target.value.length > 100){
             count.style.color ="rgb(255, 200, 0)";
         }
         if(e.target.value.length > 140) {
             count.style.color ="rgb(255, 0, 0)";
-            document.getElementById("botaoEnviar").disabled = true;
+            buttonElement.disabled = true;
         }
         if(e.target.value.length <= 140) {
-            document.getElementById("botaoEnviar").disabled = false;
+            buttonElement.disabled = false;
         }
         if(e.target.value.length <= 100) {
             count.style.color ="rgb(0, 0, 0)";
@@ -22,4 +34,3 @@ function caracteres(e) {
         count.textContent =  140 - e.target.value.length;
         }
 }
-
