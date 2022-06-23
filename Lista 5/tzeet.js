a8cbdd1748f6b49e4081c34776d7e0d657437f08
André Silva Cavalcanti de Albuquerque <103077110+drznn@users.noreply.github.com>
2022-06-23 20:13:46 -0300

    Update tzeet.js

--- Lista 5/tzeet.js
+++ Lista 5/tzeet.js
@@ -36,8 +36,8 @@ function caracteres(e, id) {
 }
 
 function validaEmail() {
-    usuario = document.getElementById("inputEmail1").value.substring(0, document.getElementById("inputEmail1").value.indexOf("@"));
-    dominio = document.getElementById("inputEmail1").value.substring(document.getElementById("inputEmail1").value.indexOf("@") + 1, document.getElementById("inputEmail1").value.length);
+    let usuario = document.getElementById("inputEmail1").value.substring(0, document.getElementById("inputEmail1").value.indexOf("@"));
+    let dominio = document.getElementById("inputEmail1").value.substring(document.getElementById("inputEmail1").value.indexOf("@") + 1, document.getElementById("inputEmail1").value.length);
 
     if ((usuario.length >= 1) &&
         (dominio.length >= 3) &&
@@ -68,21 +68,22 @@ function validaEmail() {
 }
 
 function validaSenha() {
-    senha = document.getElementById("inputPassword1").value;
-    if(senha.length < 1){
+    let senha = document.getElementById("inputPassword1").value;
+    if (senha.length < 1) {
         document.getElementById("btnEntrar").disabled = true;
         document.getElementById("inputPassword1").className = "form-control is-invalid";
         document.getElementById("msgsenha").innerHTML = "<font color='red'>A senha é Obrigatória e deve ter pelo menos 6 Caracteres </font>";
     }
-    if(senha.length >=1 && senha.length < 6) {
+    if (senha.length >= 1 && senha.length < 6) {
         document.getElementById("inputPassword1").className = "form-control is-invalid";
         document.getElementById("msgsenha").innerHTML = "<font color='red'>A senha é Obrigatória e deve ter pelo menos 6 Caracteres </font>";
         document.getElementById("btnEntrar").disabled = false;
         return false;
-    }if(senha.length >=6){
+    } if (senha.length >= 6) {
         document.getElementById("msgsenha").innerHTML = "";
         document.getElementById("inputPassword1").className = "form-control is-valid";
         if (validaEmail()) {
+            console.log("teste")
             document.getElementById("btnEntrar").disabled = false;
         } else {
             document.getElementById("btnEntrar").disabled = true;
@@ -92,8 +93,8 @@ function validaSenha() {
 }
 
 function confirmaSenha() {
-    senha1 = document.getElementById("inputPassword1");
-    senha2 = document.getElementById("inputPassword2");
+    let senha1 = document.getElementById("inputPassword1");
+    let senha2 = document.getElementById("inputPassword2");
 
     if (senha1.value === senha2.value) {
         document.getElementById("msgsenha2").innerHTML = "";
@@ -104,16 +105,76 @@ function confirmaSenha() {
     }
 }
 
-function validaNome(){
-    nome = document.getElementById("formGroupExampleInput").value;
-    if(nome.length < 1){
+function validaNome() {
+    let nome = document.getElementById("formGroupExampleInput").value;
+    if (nome.length < 1) {
         document.getElementById("formGroupExampleInput").className = "form-control is-invalid";
         document.getElementById("msgnome").innerHTML = "<font color='red'>Por favor, Informe um nome de usuário </font>";
         return false;
-    }else{
+    } else {
         document.getElementById("msgnome").innerHTML = "";
         document.getElementById("formGroupExampleInput").className = "form-control is-valid";
         return true;
     }
 
+}
+
+function addTabelaDia(value) {
+    const meses = value;
+
+    var select = document.getElementById("dia");
+
+    let flag = false;
+    while (select.length > 1) {
+        if (select.children[0].innerHTML === 'Dia' && !flag ){
+            flag = true;
+            continue;
+        }    
+        select.removeChild(select.lastChild);
+    }
+
+    if (meses === '4' || meses === '6' || meses === '9' || meses === '11') {
+        for (let index = 1; index < 31; index++) {
+            $("#dia option").eq(0).after($("<option>Uma novo dia</option>").text(index));
+        }
+    } else if (meses === '2') {
+
+        for (let index = 1; index <= 29; index++) {
+            $("#dia option").eq(0).after($("<option>Uma novo dia</option>").text(index));
+        }
+    } else {
+        for (let index = 1; index <= 31; index++) {
+            $("#dia option").eq(0).after($("<option>Uma novo dia</option>").text(index));
+        }
+    }
+
+}
+
+
+function addTabelaAno(value) {
+    let mes = document.getElementById("mes").value;
+    let dia = value;
+
+    var select = document.getElementById("ano");
+
+    let flag = false;
+    while (select.length > 1) {
+        if (select.children[0].innerHTML === 'Ano' && !flag ){
+            flag = true;
+            continue;
+        }    
+        select.removeChild(select.lastChild);
+    }
+
+    if (mes === '2' && dia === "29") {
+        for (let index = 1985; index <= 2022; index++) {
+            if (index % 4 == 0) {
+                $("#ano option").eq(0).after($("<option>Uma novo ano</option>").text(index));
+            }
+        }
+    } else {
+        for (let index = 1985; index <= 2022; index++) {
+            $("#ano option").eq(0).after($("<option>Uma novo ano</option>").text(index));
+        }
+    }
 }
